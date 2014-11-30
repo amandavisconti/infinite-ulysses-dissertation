a43ede5e77c01f553891f6c0abf5e89b81a4be61
avisconti <amandavisconti@2551276.no-reply.drupal.org>
2014-11-30 18:51:05 -0500

    Fixed Chrome AJAX submit; switched GET->POST
    Together with changes via UI, fixed issue where Chrome only was
    refreshing the entire page on highlight-click instead of just updating
    the sidebar block (see
    https://basecamp.com/2731370/projects/6892159/todos/139848154).
    Switched from GET to POST for various reasons (see
    https://basecamp.com/2731370/projects/6892159/todos/139718743).

---
+++ Annotation Sidebar Work/modules/custom/highlighted_annos/highlighted_annos.js
@@ -0,0 +1,22 @@
+(function ($) {
+  Drupal.behaviors.ViewsExposedFormFix = {
+    attach: function() {
+      if (Drupal.settings && Drupal.settings.views && Drupal.settings.views.ajaxViews) {
+        $.each(Drupal.settings.views.ajaxViews, function(i, settings) {
+          // This matches the logic in Drupal.views.ajaxView.prototype.attachExposedFormAjax.
+          var exposed_form = $('form#views-exposed-form-'+ settings.view_name.replace(/_/g, '-') + '-' + settings.view_display_id.replace(/_/g, '-'));
+          exposed_form.once('views-exposed-form-fix', function() {
+            var button = $('input[type=submit], button[type=submit], input[type=image]', exposed_form);
+            button = button[0];
+            // This will catch browsers that don't activate the submit button when pressing enter in the form.
+            exposed_form.submit(function (event) {
+              button.click();
+              event.preventDefault();
+              return false;
+            });
+          })
+        });
+      }
+    }
+  };
+})(jQuery);
