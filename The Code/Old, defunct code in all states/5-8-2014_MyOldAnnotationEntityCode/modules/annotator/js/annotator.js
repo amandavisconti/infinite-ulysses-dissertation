83ab9494300bab15653dcce139805abe000d2b4b
avisconti <amandavisconti@2551276.no-reply.drupal.org>
2015-02-06 17:19:43 -0500

    Old code from wrestling with the annotation entity

---
+++ The Code/Old, defunct code in all states/5-8-2014_MyOldAnnotationEntityCode/modules/annotator/js/annotator.js
@@ -0,0 +1,7 @@
+(function ($) {
+  Drupal.behaviors.annotator = {
+    attach: function (context, settings) {
+      Drupal.Annotator = $(Drupal.settings.annotator.element).annotator();
+    }
+  };
+})(jQuery);
