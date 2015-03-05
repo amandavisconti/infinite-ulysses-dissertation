feda733ffa3bcc8188c819f4d5d3c91ac4211e95
avisconti <amandavisconti@2551276.no-reply.drupal.org>
2015-03-05 09:39:16 -0500

    Last commit of retired Throwaway theme
    This theme was replaced with the new LemonSoap theme.

--- The Code/themes/throwaway/js/throwaway.js
+++
@@ -1,8 +0,0 @@
-(function($){
-  // Shorthand for $( document ).ready()
-  $(function() {
-    // this will activate on the targets
-    $('*[data-toggle="tooltip"]').tooltip();
-    $('*[data-toggle="popover"]').popover();
-  });
-})(jQuery)
